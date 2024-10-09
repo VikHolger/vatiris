fe21786cafb7a11dfa8150e6dc0c1a4a72f92805
Max Larsson Kuhla <maxlk96@gmail.com>
2024-10-09 21:36:16 +0200

    PLS settings moved, added settings store
    #44

--- frontend/src/stores/settings.ts
+++ frontend/src/stores/settings.ts
@@ -5,6 +5,13 @@ export const useSettingsStore = defineStore("settings", () => {
     const windowSnapping = ref(true)
     const metreportFlash = ref(true)
     const metsensorFlash = ref(false)
+    const enablePLS = ref(false)
+    const plsLogic = ref("CID")
+    const useVatsimConnect = ref(true)
+    const cid1 = ref("")
+    const cid2 = ref("")
+    const position1 = ref("")
+    const position2 = ref("")
 
     if ("settings" in localStorage) {
         try {
@@ -12,17 +19,42 @@ export const useSettingsStore = defineStore("settings", () => {
             if ("windowSnapping" in settings) windowSnapping.value = settings.windowSnapping
             if ("metreportFlash" in settings) metreportFlash.value = settings.metreportFlash
             if ("metsensorFlash" in settings) metsensorFlash.value = settings.metsensorFlash
+            if ("enablePLS" in settings) enablePLS.value = settings.enablePLS
+            if ("plsLogic" in settings) plsLogic.value = settings.plsLogic
+            if ("useVatsimConnect" in settings) useVatsimConnect.value = settings.useVatsimConnect
+            if ("cid1" in settings) cid1.value = settings.cid1
+            if ("cid2" in settings) cid2.value = settings.cid2
+            if ("position1" in settings) position1.value = settings.position1
+            if ("position2" in settings) position2.value = settings.position2
         } catch (e: any) {
             console.error("Failed to parse settings", e)
             delete localStorage.settings
         }
     }
 
-    watch([windowSnapping, metsensorFlash], () => {
+    watch([
+        windowSnapping,
+        metreportFlash,
+        metsensorFlash,
+        enablePLS,
+        plsLogic,
+        useVatsimConnect,
+        cid1,
+        cid2,
+        position1,
+        position2
+    ], () => {
         localStorage.settings = JSON.stringify({
             windowSnapping: windowSnapping.value,
             metreportFlash: metreportFlash.value,
-            metsensorFlash: metsensorFlash.value
+            metsensorFlash: metsensorFlash.value,
+            enablePLS: enablePLS.value,
+            plsLogic: plsLogic.value,
+            useVatsimConnect: useVatsimConnect.value,
+            cid1: cid1.value,
+            cid2: cid2.value,
+            position1: position1.value,
+            position2: position2.value
         })
     })
 
@@ -30,5 +62,12 @@ export const useSettingsStore = defineStore("settings", () => {
         windowSnapping,
         metreportFlash,
         metsensorFlash,
+        enablePLS,
+        plsLogic,
+        useVatsimConnect,
+        cid1,
+        cid2,
+        position1,
+        position2,
     }
 })
